2c671ffb981ddeeaa66232b785b1ac118df52b9e
TranDucDuy_ЧанДыкЗюи <tranduybthv@gmail.com>
2023-10-09 14:10:01 +0300

    From Dev AT, modified

---
+++ README.md
@@ -0,0 +1,12 @@
+# NEXTJS - Ecommerce
+
+> Build a E-commerce Full Stack with Nextjs + Mongodb + React Hook + Bootstrap 4
+> Instructions on how to use nextjs in conjunction with mongodb to build a basic e-commerce website.
+
+Nguồn:
+
+## Demo: https://nextjs-ecommerce-six.vercel.app/
+
+## Author: Dev AT
+
+## Youtube tutorials: https://www.youtube.com/watch?v=UsEN7LGefhA&list=PLs4co9a6NhMzf-5tIWfpzDCgdMyg-CceX
---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://...",
+        "MONGODB_URL": "mongodb+srv:...",
+        "ACCESS_TOKEN_SECRET": "TOKEN_SECRET",
+        "REFRESH_TOKEN_SECRET": "TOKEN_SECRET",
+        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+        "CLOUD_UPDATE_PRESET": "CLOUD_UPDATE_PRESET",
+        "CLOUD_NAME": "CLOUD_NAME",
+        "CLOUD_API": "https://api.cloudinary.com/ CLOUD_API"
+    }
+}
